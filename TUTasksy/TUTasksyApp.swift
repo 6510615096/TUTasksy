c876cd049bc288a4d7be1f348b1582a1c0748877
Nutpupicha <6510615096@student.tu.ac.th>
2025-04-30 13:45:47 +0700

    Initial Commit

---
+++ TUTasksy/TUTasksyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUTasksyApp.swift
+//  TUTasksy
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 30/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TUTasksyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
